0438fab919af80d7eb9da876717ea06fa05189df
Bitseat <bitseatt@gmail.com>
2019-11-29 13:01:29 +0300

    change path

--- opencog/atomese/scm/asmoses.scm
+++ opencog/atomese/scm/asmoses.scm
@@ -5,7 +5,8 @@
 )
 
 ; Load the C library; this calls the nameserver to load the types.
-(load-extension "libasmoses_types" "asmoses_types_init")
+(load-extension "/home/bitseat/opencog/asmoses/cmake-build-debug/opencog/atomese/atom_types/libasmoses_types.so" "asmoses_types_init")
+;(load-extension (string-append opencog-ext-path-asmoses-types "libasmoses-types") "atom_types_init")
 
-; Load various parts....
-(load "atomese/atom_types/atom_types.scm")
+; Load various parts.... pritmitive load path
+(load "/home/bitseat/opencog/asmoses/cmake-build-debug/opencog/atomese/atom_types/atom_types.scm")
