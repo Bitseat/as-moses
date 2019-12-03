dd408f80cb1ba3b46b9b145fba5117e45c890801
Bitseat <bitseatt@gmail.com>
2019-12-03 15:39:53 +0300

    Fix configuration error

--- opencog/atomese/scm/asmoses.scm
+++ opencog/atomese/scm/asmoses.scm
@@ -5,7 +5,8 @@
 )
 
 ; Load the C library; this calls the nameserver to load the types.
-(load-extension "libasmoses_types" "asmoses_types_init")
+(load-extension "/usr/local/lib/opencog/libasmoses_types" "asmoses_types_init")
 
 ; Load various parts....
-(load "asmoses/types/atom_types.scm")
+(load-from-path "opencog/atomese/atom_types.scm")
+(export)
